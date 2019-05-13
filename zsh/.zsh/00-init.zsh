d6e9e7e9b5ebeefd86e291de68c1a606c90844aa
Robert Fogarty <rgaf@purgator.io>
2019-05-13 09:43:39 -0400

    Add more IPA stuff; remove old work-related aliases & env vars

--- zsh/.zsh/00-init.zsh
+++ zsh/.zsh/00-init.zsh
@@ -4,11 +4,6 @@ export EDITOR="nvim"
 export GIT_EDITOR="nvim"
 export XDG_CONFIG_HOME="~/.config"
 
-export PSIWARE_INSTANCE_ID="RGAF Psiware Dev"
-export PSIWARE_CLIENT_URL="https://psiware.purgator.io"
-export PSIWARE_IDENTICON_TMP_DIR="tmp/identicons"
-export GRAPHQL_ENDPOINT="https://localhost:5000/graphql"
-
 export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 eval "$(rbenv init -)"
 
--- zsh/.zsh/05-aliases.zsh
+++ zsh/.zsh/05-aliases.zsh
@@ -8,7 +8,6 @@ alias home="cd ~"
 alias mkdir="mkdir -p"
 
 alias df="cd ~/dotfiles"
-alias p="cd ~/Projects"
 alias g="git"
 
 alias dir="cd"
@@ -23,6 +22,7 @@ alias envim="nvim ~/dotfiles/nvim/.config/nvim/init.vim"
 alias szsh="source ~/.zshrc"
 
 # Personal projects
+alias prj="cd ~/projects"
 alias wyrd="cd ~/projects/wyrd"
 alias pkl="cd ~/projects/proto-kasso-lengic"
 
@@ -30,10 +30,5 @@ alias pkl="cd ~/projects/proto-kasso-lengic"
 alias rlc="rails console"
 alias rls="rails server"
 
-# TTech
-alias psia="cd ~/work/psiware-api"
-alias slpr="cd ~/work/slp/rails"
-alias slpp="cd ~/work/slp/reporting"
-
-export SLP_SOFFICE="/usr/bin/soffice"
-export SLP_CONVERT="/usr/bin/convert"
+# Work-related
+alias work="cd ~/work"
